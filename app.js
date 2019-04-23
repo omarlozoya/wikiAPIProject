0c1557aad1f7e2f1ef3f34dd2c56b090fcbd7742
Omar Lozoya <23390638+omarlozoya@users.noreply.github.com>
2019-04-23 10:37:30 -0700

    Added the delete route for /articles

--- app.js
+++ app.js
@@ -37,6 +37,16 @@ app.post('/articles', (req, res) => {
     });
 });
 
+app.delete('/articles', (req, res) => {
+    Article.deleteMany((err) => {
+        if (!err) {
+            res.send('Successfully deleted all articles!');
+        } else {
+            res.send(err);
+        }
+    });
+});
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server started on port 3000");
 });
